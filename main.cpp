42a7bdfcaab27ea7c4081a895eae542a21541504
Yohay Artzi <yohayartzi@gmail.com>
2022-06-12 18:40:22 +0300

    one file

--- main.cpp
+++ main.cpp
@@ -3,12 +3,93 @@
 #include <string>
 #include <vector>
 #include <thread>
-#include "BoundedQueue.h"
-#include "UnboundedQueue.h"
 #include <string.h>
 #include <unistd.h>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
 
 using namespace std;
+
+class BoundedQueue{
+    sem_t full;
+    sem_t empty;
+    mutex mtx;
+    queue<string> q;
+
+public:
+    BoundedQueue(int size){
+        sem_init(&this->full, 0, 0);
+        sem_init(&this->empty, 0, size);
+    }
+
+    // atomic insert
+    void insert (string str){
+        sem_wait(&this->empty);
+        mtx.lock();
+        this->q.push(str);
+        mtx.unlock();
+        sem_post(&this->full);
+    }
+
+    // atomic remove
+    string remove () {
+        sem_wait(&this->full);
+        mtx.lock();
+        string str = this->q.front();
+        this->q.pop();
+        mtx.unlock();
+        sem_post(&this->empty);
+
+        return str;
+    }
+
+    string front (){
+        mtx.lock();
+        string str = this->q.front();
+        mtx.unlock();
+
+        return str;
+    }
+};
+
+class UnboundedQueue{
+    sem_t full;
+    mutex mtx;
+    queue<string> q;
+
+public:
+    UnboundedQueue(){
+        sem_init(&this->full, 0, 0);
+    }
+
+    // atomic insert
+    void insert (string str){
+        mtx.lock();
+        this->q.push(str);
+        mtx.unlock();
+        sem_post(&this->full);
+    }
+
+    // atomic remove
+    string remove () {
+        sem_wait(&this->full);
+        mtx.lock();
+        string str = this->q.front();
+        this->q.pop();
+        mtx.unlock();
+
+        return str;
+    }
+
+    string front (){
+        mtx.lock();
+        string str = this->q.front();
+        mtx.unlock();
+
+        return str;
+    }
+};
 
 void checkInputScanning(vector<int> id, vector<int> cap, vector<int> size, int ce);
 
